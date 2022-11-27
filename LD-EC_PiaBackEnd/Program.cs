097543c8c4299ce609fdaa411a71af88dbf8160c
Alessandro Elizondo <alessandro.uanl@gmail.com>
2022-11-26 22:41:27 -0600

    File Upload
    Se creó el proyecto base con clase startup creada y configurada, se instalo el paquete entityframework.sqlserver y el applicationdbcontext

---
+++ LD-EC_PiaBackEnd/Program.cs
@@ -0,0 +1,14 @@
+using LD_EC_PiaBackEnd;
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+startup.Configure(app, app.Environment);
+
+app.Run();
